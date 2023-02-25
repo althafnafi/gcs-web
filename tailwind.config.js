036e8d4ab5c0a353717119f3367dbec0cf075731
Althaf <althafnaa@gmail.com>
2023-02-26 04:03:21 +0700

    revised states box

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      mono: ["Ubuntu Mono"],
+      roboto: ["Roboto"],
+      roboto_bold: ["Roboto Bold"],
+      jetbrains: ["JetBrains Mono"],
+      jetbrains_bold: ["JetBrains Mono Bold"],
+    },
     extend: {},
   },
   plugins: [require("daisyui")],
